689df0543d037b6c92b5b71d729294ce052b2b98
Md-Ruhul-Amin-Rony <adiar1234@yahoo.com>
2023-09-22 12:02:22 +0200

    first commit

---
+++ ChatAppSignalRBlazor/Server/Hubs/ChatHub.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+
+namespace ChatAppSignalRBlazor.Server.Hubs
+{
+
+
+    
+
+    public class ChatHub : Hub
+    {
+        private static Dictionary<string, string> Users = new Dictionary<string, string>();
+
+
+
+
+        public override async Task OnConnectedAsync() 
+        {
+
+            string username = Context.GetHttpContext().Request.Query["username"];
+            Users.Add(Context.ConnectionId, username);
+            //await AddMessageToChat("", "User Connected");
+            await AddMessageToChat(string.Empty, $"{username} joined the chatroom.");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception) 
+        {
+            string username = Users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+            await AddMessageToChat(string.Empty, $"{username} left the chatroom.");
+        }
+
+
+        public async Task AddMessageToChat(string user, string message) 
+        {
+            await Clients.All.SendAsync( "GetThatMessageDude", user, message); 
+        }
+
+    }
+}
